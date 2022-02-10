2c153459c84ce21ae783da38d7ba44ef7acbd7a0
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2022-02-10 20:18:33 +0530

    Delete quiz_app directory

--- quiz_app/lib/question.dart
+++
@@ -1,9 +0,0 @@
-class Question{
-  late String questionText;   // late keyword use is to use the variable in later process
-  late bool questionAnswer;
-
-  Question({ required String q,required bool a}){ // constructor
-    questionText=q;
-    questionAnswer=a;
-  }
-}
